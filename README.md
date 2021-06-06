2a085ed0f9bc668a2f1a32417fd0a20cb88af3ef
alejandro <abergasov@gmail.com>
2021-06-06 23:22:11 +0300

    config update

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # dot_files
 
+### download config
 ```shell
-# download config
 git clone https://github.com/abergasov/dot_files.git && cd dot_files && python3 set_config.py
+```
 
-# upload conf to git
+### upload conf to git
+```shell
 python3 rewrite_files.py
 ```
--- rewrite_files.py
+++ rewrite_files.py
@@ -1,5 +1,4 @@
 import os
-from os.path import isfile, join
 from pathlib import Path
 import subprocess as cmd
 import shutil
@@ -12,27 +11,25 @@ if os.path.exists(git_dir):
 executable_files = []
 
 
-def copy_file(path_src: str, path_target: str) -> bool:
-    shutil.copyfile(path_src, path_target)
-    return os.access(path_src, os.X_OK)
-
-
 target_data = [".config/i3", ".config/i3blocks", ".config/i3status", ".config/dunst", ".zshrc"]
 for i in target_data:
     src_path = home + i
     target_path = git_dir + i
-    if os.path.isdir(src_path):
-        Path(target_path).mkdir(parents=True, exist_ok=True)
-    else:
-        if copy_file(src_path, target_path) is True:
-            executable_files.append(i)
+    if not os.path.isdir(src_path):
+        shutil.copyfile(src_path, target_path)
         continue
 
-    for f in [f for f in os.listdir(src_path) if isfile(join(src_path, f))]:
-        src_file = '{0}/{1}'.format(src_path, f)
-        if os.access(src_file, os.X_OK):
-            executable_files.append("{0}/{1}".format(i, f))
-        shutil.copyfile(src_file, '{0}/{1}'.format(target_path, f))
+    if os.path.exists(target_path):
+        shutil.rmtree(target_path)
+    shutil.copytree(src_path, target_path)
+
+
+for root, dirs, files in os.walk(git_dir):
+    for file in files:
+        path = (root + os.sep + file).replace("//", "/")
+        if os.access(path, os.X_OK):
+            executable_files.append(path.replace(git_dir, ""))
+
 
 with open("executable_list.json", "w") as outfile:
     outfile.write(json.dumps(executable_files, indent=4))
